3967b0e2bda9eeb9ae42becd2d9b4c58ca57d2a4
Nibedita Sahoo <nibeditaonly@gmail.com>
2017-02-27 18:44:08 +0530

    check

---
+++ src/main/java/com/nsahoo/EmpTest.java
@@ -0,0 +1,39 @@
+package com.nsahoo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+
+/**
+ * Created by Nibedita on 2/27/2017.
+ */
+public class EmpTest {
+
+    public static void main(String args[])
+    {
+        Employee e1=new Employee(1,"Hari"26,"Bangalore");
+        Employee e2=new Employee(2,"Ritesh"27,"kolkata");
+        Employee e3=new Employee(3,"Akshaya"24,"Mumbai");
+        Employee e4=new Employee(4,"Govind"25,"Chennai");
+        Employee e5=new Employee(5,"Rajesh"23,"Hyderabad");
+
+
+        List<Employee> employees= new ArrayList<>();
+
+        employees.add(e1);
+        employees.add(e2);
+        employees.add(e3);
+        employees.add(e4);
+        employees.add(e5);
+
+        List<Employee> list1= new ArrayList<>(employees);
+        List<Employee> list2= new ArrayList<>(employees);
+
+        System.out.println("Compare by name :");
+        list1.sort((emp1,emp2) -> emp1.getName().compareTo(emp2.getName())) ;
+
+        System.out.println("Compare by id :");
+        list2.sort(comparing(Employee::getId));
+    }
+}
---
+++ src/main/java/com/nsahoo/Employee.java
@@ -0,0 +1,52 @@
+package com.nsahoo;
+
+/**
+ * Created by Nibedita on 2/27/2017.
+ */
+public class Employee {
+    private int id;
+    private String name;
+
+    public Employee(int id, String name, int age, String city) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    private  int age;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
